c56c238e2edbb6697c4ea604ed1821c24a76da44
DarkDev <darkdev_brand@riseup.net>
2022-10-15 00:10:16 +0300

    fix animation for title

--- src/components/AboutUs.svelte
+++ src/components/AboutUs.svelte
@@ -2,19 +2,25 @@
 	import Contact from './Contact.svelte';
 </script>
 
+
 <div data-aos="fade-down"
     data-aos-delay="80"
     data-aos-duration="1000"
     data-aos-easing="ease-in-out"
     data-aos-mirror="true"
     data-aos-once="false" 
-    style="max-width: 100%; width: 720px; " class="mb-12 flex max-w-sm flex-col bg-white dark:bg-gray-800 shadow-md rounded-lg border border-gray-200  text-gray-500 dark:text-gray-400 dark:border-gray-700 p-6">
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Alternatywy 5  - Dom Sąsiedzki w Kamiennym Potoku</h5>
-      <p class="font-normal text-gray-700  dark:text-gray-400 leading-tight mt-2 mb-2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      </p>
-      <p class="font-normal text-gray-700  dark:text-gray-400 leading-tight mt-2 mb-2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      </p>
-      <Contact />
+    style=" width: 720px;">
+
+    <h2 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl text-center">O nas</h2>
+    <div style="max-width: 100%; " class="mb-12 flex max-w-sm flex-col bg-white shadow-md rounded-lg border border-gray-200  text-gray-500 p-6">
+        
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Alternatywy 5  - Dom Sąsiedzki w Kamiennym Potoku</h5>
+        <p class="font-normal text-gray-700  dark:text-gray-400 leading-tight mt-2 mb-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+        <p class="font-normal text-gray-700  dark:text-gray-400 leading-tight mt-2 mb-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+        <Contact />
+    </div>
 </div>
