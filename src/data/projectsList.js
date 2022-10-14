3bc7203006bdf1d59833ee9ba0bad6e0b8944b7e
DarkDev <darkdev_brand@riseup.net>
2022-10-15 00:12:11 +0300

    refactorint export data

---
+++ src/data/projectsList.js
@@ -0,0 +1,54 @@
+const projectsList = [
+	{
+		align: 'start',
+		title: '🪓 Warsztat stolarski ',
+		anchor: 'wood',
+		description:
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+	},
+	{
+		align: 'end',
+		title: '💻 44Hackerspace ',
+		anchor: 'hackerspace',
+		description:
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+	},
+	{
+		align: 'start',
+		title: '📷 Warsztaty fotograficzne ',
+		anchor: 'photo',
+		description:
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+	},
+	{
+		align: 'end',
+		title: '🏓 Turnieje ping-pongowe ',
+		anchor: 'ping-pong',
+		description:
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+	},
+	{
+		align: 'start',
+		title: '🚲 Warsztat rowerowy ',
+		anchor: 'bicycle',
+		description:
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+	},
+	{
+		align: 'end',
+		title: '♟️ Turnieje szachowe ',
+		anchor: 'chess',
+		description:
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+	},
+	{
+		align: 'start',
+		title: '🛠 Naprawa elektronarzędzi ',
+		anchor: 'repair',
+		description:
+			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+	}
+];
+
+
+export default projectsList;
