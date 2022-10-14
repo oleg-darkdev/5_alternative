26754a6defb93bb71c9b21a1821fae998ddd4d64
DarkDev <darkdev_brand@riseup.net>
2022-10-14 21:03:27 +0300

    add global css & initialise aos.js

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,25 @@
 <script>
 	import '../app.css';
 	import 'aos/dist/aos.css'; 
-
+	import AOS from 'aos';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		AOS.init();
+	});   
+	 
 	import Header from '../components/Header.svelte';
 	import Footer from '../components/Footer.svelte';
 </script>
+
 <Header />
 <slot />
 <Footer />
+
+
+<style>
+	:global(.flex-align-center) {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+</style>
