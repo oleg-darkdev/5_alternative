52ec270b297efe6f974ab6b31fd45d43bc0d7cd8
DarkDev <darkdev_brand@riseup.net>
2022-10-15 00:12:35 +0300

    add invidual anchors for navigation on footer

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -1,16 +1,17 @@
 <script>
   import { Button } from 'flowbite-svelte';
-  export let align, title, img, description;
+  export let project;
 </script>
 
-<div  data-aos="flip-left" style="display: flex; justify-content: flex-{align};" class=" lg:w-6/12 sm:w-8/12 mt-2 mb-2">
+<div  data-aos="flip-left" style="display: flex; justify-content: flex-{project.align};" class=" lg:w-6/12 sm:w-8/12 mt-2 mb-2">
+  <a name={project.anchor}></a> 
   <!-- <Card img={img} href="/" horizontal> -->
-  <div class="flex sm:max-w-xl flex-col md:flex-row h-auto md:max-w-xl bg-white dark:bg-gray-800 shadow-md text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-700">
+  <div class="flex sm:max-w-xl flex-col md:flex-row h-auto md:max-w-xl bg-white  shadow-md text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-700">
     <!-- <img class="rounded-t-lg object-cover w-full h-auto md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://www.w3schools.com/howto/img_girl.jpg" alt=""> -->
     <div class="p-4 project-card rounded-lg">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{project.title}</h5>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-      {description}
+      {project.description}
     </p>
     <Button gradient color="pinkToOrange" size="xl">More</Button>
     </div>
