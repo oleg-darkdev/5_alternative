832871452f156e73745e9ddd4dfdc5dbc7647a8a
DarkDev <darkdev_brand@riseup.net>
2022-10-14 15:03:00 +0300

    add social contact data

---
+++ src/components/contact/Social.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Avatar } from "flowbite-svelte";
+  import AOS from 'aos';
+  import { onMount } from 'svelte';
+  onMount(() => {
+      AOS.init();
+  });       
+
+  export let social;
+</script>
+
+<div
+    data-aos="flip-left"
+    data-aos-delay="50"
+    data-aos-duration="1200"
+    data-aos-easing="ease-in-out"
+    data-aos-mirror="true"
+    data-aos-once="false" 
+    on:click={() => {window.open(social.link, '_blank').focus(); console.log('asd')}}> 
+  <Avatar class="m-4 shadow-xl " src={social.logoImg}   rounded size="lg" />
+</div>
---
+++ src/data/social.js
@@ -0,0 +1,68 @@
+const socialLinks = [
+	{
+		name: 'Instagram',
+		logoImg: '/social/instagram.png',
+		link: '',
+		logoSvg: ''
+	},
+	{
+		name: 'Facebook',
+		logoImg: '/social/facebook.png',
+		link: '',
+		logoSvg: ''
+	},
+	{
+		name: 'Telegram',
+		logoImg: '/social/telegram.png',
+		link: '',
+		logoSvg: ''
+	},
+	{
+		name: 'Twitter',
+		logoImg: '/social/twiiter.png',
+		link: '',
+		logoSvg: ''
+	},
+
+	{
+		name: 'Linkedin',
+		logoImg: '/social/linkedin.png',
+		link: '',
+		logoSvg: ''
+	},
+	{
+		name: 'Patronite',
+		logoImg: '/social/patronite.png',
+		link: '',
+		logoSvg: ''
+	},
+	// {
+	// 	name: 'Github',
+	// 	logoImg: '',
+	// 	link: '',
+	// 	logoSvg: ''
+	// },
+	// {
+	// 	name: 'Etsy',
+	// 	logoImg: '',
+	// 	link: '',
+	// 	logoSvg: ''
+	// },
+	// {
+	// 	name: 'Event brite',
+	// 	logoImg: '',
+	// 	link: '',
+	// 	logoSvg: ''
+	// },
+	// {
+	// 	name: 'Meetup',
+	// 	logoImg: '',
+	// 	link: '',
+	// 	logoSvg: ''
+	// }
+];
+
+
+
+
+export default socialLinks
