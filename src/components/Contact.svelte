19feb76477ca9f83aede0dae723c45f326ae7e12
DarkDev <darkdev_brand@riseup.net>
2022-10-14 15:03:40 +0300

    alfa v. contacts data wrap

---
+++ src/components/Contact.svelte
@@ -0,0 +1,19 @@
+<script>
+    import SocialAvatars from './contact/Social.svelte';
+    
+    import socialData from '../data/social';
+</script>
+<div class=" flex flex-wrap justify-center flex-row  p-6 rounded-3xl bg-gray-800 ">
+    {#each socialData as social}
+        <SocialAvatars {social}/>
+    {/each}
+</div>
+
+<style>
+    div {
+        background-image: url(/social/social.png);  
+        background-position: center; 
+        background-repeat: no-repeat;  
+        background-size: cover;
+    }
+</style>
