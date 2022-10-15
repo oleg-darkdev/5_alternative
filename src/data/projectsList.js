4c0dec5ff960e8fb96fe2ddb3b63075f6db80b45
DarkDev <darkdev_brand@riseup.net>
2022-10-15 16:46:55 +0300

    add varibale for show/hide btn

--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -3,6 +3,7 @@ const projectsList = [
 		align: 'start',
 		title: '🪓 Warsztat stolarski ',
 		anchor: 'wood',
+		showMoreInfo: false,
 		description:
 			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
 	},
@@ -10,6 +11,7 @@ const projectsList = [
 		align: 'end',
 		title: '💻 44Hackerspace ',
 		anchor: 'hackerspace',
+		showMoreInfo: false,
 		description:
 			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
 	},
@@ -17,27 +19,30 @@ const projectsList = [
 		align: 'start',
 		title: '📷 Warsztaty fotograficzne ',
 		anchor: 'photo',
+		showMoreInfo: false,
 		description:
 			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
 	},
 	{
 		align: 'end',
-		title: '🏓 Turnieje ping-pongowe ',
+		title: '🏅 Turnieje sportowe',
 		anchor: 'ping-pong',
-		description:
-			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
+		showMoreInfo: false,
+		description: '🏓 Turnieje ping-pongowe, ♟️ Turnieje szachowe '
 	},
 	{
 		align: 'start',
 		title: '🚲 Warsztat rowerowy ',
 		anchor: 'bicycle',
+		showMoreInfo: false,
 		description:
 			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
 	},
 	{
 		align: 'end',
-		title: '♟️ Turnieje szachowe ',
-		anchor: 'chess',
+		title: '🎲 Wypożyczalnia gier planszowych',
+		anchor: 'boardgames',
+		showMoreInfo: false,
 		description:
 			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
 	},
@@ -45,6 +50,7 @@ const projectsList = [
 		align: 'start',
 		title: '🛠 Naprawa elektronarzędzi ',
 		anchor: 'repair',
+		showMoreInfo: false,
 		description:
 			'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
 	}
