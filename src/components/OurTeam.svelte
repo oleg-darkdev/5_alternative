433dfb4624c7d2feb5c7968ed5a291be52d635a3
DarkDev <darkdev_brand@riseup.net>
2022-10-15 16:48:30 +0300

    fix to mobile

--- src/components/OurTeam.svelte
+++ src/components/OurTeam.svelte
@@ -3,7 +3,7 @@
 	import membersList from '../data/membersList';
 </script>
 
-<div class="flex-align-center m-4">
+<div class="flex-align-center m-4 lg:w-8/12  sm:w-12/12">
 	{#each membersList as memberData}
 		<MemberCard {memberData} />
 	{/each}
