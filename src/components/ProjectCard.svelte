99683a0e7c2c8ced6cb232af488079b308a4a2a2
DarkDev <darkdev_brand@riseup.net>
2022-10-15 16:48:22 +0300

    clear css class for dark-mode, add more/hide functional for button

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -9,22 +9,27 @@
 	class=" lg:w-6/12 sm:w-8/12 mt-2 mb-2"
 >
 	<a name={project.anchor} />
-	<!-- <Card img={img} href="/" horizontal> -->
 	<div
 		class="flex sm:max-w-xl flex-col md:flex-row h-auto md:max-w-xl bg-white  shadow-md text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-700"
 	>
 		<!-- <img class="rounded-t-lg object-cover w-full h-auto md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://www.w3schools.com/howto/img_girl.jpg" alt=""> -->
 		<div class="p-4 project-card rounded-lg">
-			<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+			<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
 				{project.title}
 			</h5>
-			<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+			<p class="mb-2 font-normal text-gray-700 dark:text-gray-400 leading-tight">
 				{project.description}
 			</p>
-			<Button gradient color="pinkToOrange" size="xl">More</Button>
+			{#if project.showMoreInfo}
+				<div class="mb-3">
+					<p class="mb-2 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					</p>
+				</div>
+			{/if}
+			<Button gradient color="pinkToOrange" on:click={() => project.showMoreInfo = true} size="xl">More</Button>
 		</div>
 	</div>
-	<!-- </Card> -->
 </div>
 
 <style>
