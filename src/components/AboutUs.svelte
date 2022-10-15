a944a1ca5f30525940f43d7441cf04cb9fecc557
DarkDev <darkdev_brand@riseup.net>
2022-10-15 16:44:06 +0300

    refactoring, add text, fir to mobile

--- src/components/AboutUs.svelte
+++ src/components/AboutUs.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Contact from './Contact.svelte';
+	import SocialMedia from './SocialMedia.svelte';
 </script>
 
 <div
@@ -9,7 +9,8 @@
 	data-aos-easing="ease-in-out"
 	data-aos-mirror="true"
 	data-aos-once="false"
-	style=" width: 720px;"
+	style="max-width: 720px;"
+	class="lg:w-7/12  sm:w-12/12"
 >
 	<h2
 		class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl text-center"
@@ -20,14 +21,11 @@
 		style="max-width: 100%; "
 		class="mb-12 flex max-w-sm flex-col bg-white shadow-md rounded-lg border border-gray-200  text-gray-500 p-6"
 	>
-		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
 			Alternatywy 5 - Dom Sąsiedzki w Kamiennym Potoku
 		</h5>
 		<p class="font-normal text-gray-700  dark:text-gray-400 leading-tight mt-2 mb-2">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-			voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			Sąsiedzka przestrzeń oferująca spotkania, wernisaże, dyskusje, warsztaty: rękodzieła, majsterkowania, muzyczne. 
 		</p>
 		<p class="font-normal text-gray-700  dark:text-gray-400 leading-tight mt-2 mb-2">
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
@@ -35,6 +33,6 @@
 			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
 			voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 		</p>
-		<Contact />
+		<SocialMedia />
 	</div>
 </div>
